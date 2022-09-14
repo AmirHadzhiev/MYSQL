6f0e879cddf315a60a51a358312c9dc042b1c00f
AmirHadzhiev <104004036+AmirHadzhiev@users.noreply.github.com>
2022-09-14 02:30:04 -1100

    Add files via upload

---
+++ DataDefinitionAndDataTypes.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE `gamebar`;
+
+CREATE TABLE `gamebar`.`employees`(
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+   `first_name`  VARCHAR(100) NOT NULL,
+    `last_name`  VARCHAR (100) NOT NULL
+) ;
+
+CREATE TABLE `gamebar`.`categories` (
+   `id` INT AUTO_INCREMENT PRIMARY KEY,
+	`name` VARCHAR(100) NOT NULL
+) ;
+CREATE TABLE `gamebar`.`products` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name`  VARCHAR(100) NOT NULL,
+   `category_id`  INT NOT NULL
+) ;
+
+INSERT INTO `gamebar`.`employees`( `first_name`, `last_name`)VALUES ( "Pesho","Pesho");
+INSERT INTO  `gamebar`.`employees`( `first_name`, `last_name`)VALUES ( "Gosho","Gosho"),("Gergana","Gergana");
+
+ALTER TABLE `gamebar`.`employees` ADD `middle_name` VARCHAR(100);
+
+ALTER TABLE `gamebar`.`products`
+ADD CONSTRAINT `fk_products_categories`
+FOREIGN KEY `products`(`category_id`)
+REFERENCES `categories`(`id`);
+
+ALTER TABLE `gamebar`.`employees`MODIFY COLUMN `middle_name` VARCHAR(100);
+
